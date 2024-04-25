3b997356dc2cfe4392439942a6a4a590544b3249
Danny <carey.d@ntlworld.com>
2024-04-25 20:01:45 +0100

    added delete job functionality. added react-toastify.

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,45 @@
-import {
-	Route,
-	createBrowserRouter,
-	createRoutesFromElements,
-	RouterProvider
-} from "react-router-dom";
+import {Route, createBrowserRouter, createRoutesFromElements, RouterProvider} from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import MainLayout from "./layouts/MainLayout";
 import JobsPage from "./pages/JobsPage";
 import NotFoundPage from "./pages/NotFoundPage";
 import JobPage, {jobLoader} from "./pages/JobPage";
+import AddJobPage from "./pages/AddJobPage";
+
+
+
+const App = () => {
+	
+	const addJob = async (newJob) => {
+		const res = await fetch('/api/jobs', {
+			method: 'POST',
+			headers: {
+				'Content-Type': 'application/json',
+			},
+			body: JSON.stringify(newJob),
+		});
+		return res;
+	};
+
+	const deleteJob = async (id) => {
+		const res = await fetch(`/api/jobs/${id}`, {
+			method: "DELETE",
+		});
+		return res;
+	}
 
 const router = createBrowserRouter(
 	createRoutesFromElements(
 		<Route path="/"element={<MainLayout />}>
 			<Route index element={<HomePage />} />
 			<Route path='/jobs' element={<JobsPage />} />
-			<Route path='/jobs/:id' element={<JobPage />} loader={jobLoader} />
+			<Route path='/add-job' element={<AddJobPage addJobSubmit={addJob}/>} />
+			<Route path='/jobs/:id' element={<JobPage  deleteJob={deleteJob}/>} loader={jobLoader} />
 			<Route path='*' element={<NotFoundPage />} />
 		</Route>
 	)
 );
-
-const App = () => {
+	
 	return (
 		<>
 			<RouterProvider router={router} />
